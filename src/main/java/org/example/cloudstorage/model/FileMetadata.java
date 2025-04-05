5d0c55785ed44ad2fc4c02a40798435b1e194522
kalush666 <jonathan.kalush@gmail.com>
2025-04-05 21:26:57 +0300

    setting up Metadata Entity

---
+++ src/main/java/org/example/cloudstorage/model/FileMetadata.java
@@ -0,0 +1,68 @@
+package org.example.cloudstorage.model;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "file_metadata")
+public class FileMetadata {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fileId;
+    private String originalName;
+    private int chunkIndex;
+    private String chunkPath;
+    private LocalDateTime uploadedAt = LocalDateTime.now();
+
+    public int getChunkIndex() {
+        return chunkIndex;
+    }
+
+    public void setChunkIndex(int chunkIndex) {
+        this.chunkIndex = chunkIndex;
+    }
+
+    public String getChunkPath() {
+        return chunkPath;
+    }
+
+    public void setChunkPath(String chunkPath) {
+        this.chunkPath = chunkPath;
+    }
+
+    public String getFileId() {
+        return fileId;
+    }
+
+    public void setFileId(String fileId) {
+        this.fileId = fileId;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOriginalName() {
+        return originalName;
+    }
+
+    public void setOriginalName(String originalName) {
+        this.originalName = originalName;
+    }
+
+    public LocalDateTime getUploadedAt() {
+        return uploadedAt;
+    }
+
+    public void setUploadedAt(LocalDateTime uploadedAt) {
+        this.uploadedAt = uploadedAt;
+    }
+}
